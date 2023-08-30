6bcaa5afc61099fe7a6b553b70376930aaeafe0b
Martino Facchin <m.facchin@arduino.cc>
2023-08-30 10:12:01 +0200

    Fix compile script

--- compile.sh
+++ compile.sh
@@ -1,8 +1,20 @@
-# don't forget to patch
-# -/* Board has bootloader */
-# -FLASH_IMAGE_START = 0x10000;
-# in hw/bsp/ra/boards/.../*.ld
+#!/bin/bash
 
-TINYUSB_ROOT=path_to_tinyusb_repo make -f Makefile.wifi  -j8
-TINYUSB_ROOT=path_to_tinyusb_repo make -f Makefile.c33  -j8
-TINYUSB_ROOT=path_to_tinyusb_repo make -f Makefile.minima  -j8
+if [ x$TINYUSB_ROOT == x ]; then
+echo Please set TINYUSB_ROOT env variable
+exit 1
+fi
+
+PROJECT_NAME=`basename $PWD`
+
+mkdir distrib
+
+make -f Makefile.wifi  -j8
+mv _build/uno_r4/$PROJECT_NAME.hex distrib/dfu_wifi.hex
+rm -rf _build
+make -f Makefile.minima  -j8
+mv _build/uno_r4/$PROJECT_NAME.hex distrib/dfu_minima.hex
+rm -rf _build
+make -f Makefile.c33  -j8
+mv _build/portenta_c33/$PROJECT_NAME.hex distrib/dfu_c33.hex
+rm -rf _build
